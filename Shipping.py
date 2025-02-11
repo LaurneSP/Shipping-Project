b85dc10d23f87cf9f8f18110094abd70d1aae0f3
LaurneSP <115570527+LaurneSP@users.noreply.github.com>
2025-02-10 20:58:37 -0500

    Create Shipping.py

---
+++ Shipping.py
@@ -0,0 +1,43 @@
+customer_basket_cost = 101
+customer_basket_weight = 44
+
+if customer_basket_cost > 100:
+  print(customer_basket_cost)
+else:
+  shipping = 1.20 * customer_basket_weight
+  total = customer_basket_cost + shipping
+  print(total)
+
+  $CustomerBasketCost = 34
+$CustomerBasketWeight = 44
+
+If($CustomerBasketCost -gt 100)
+{Write-Host "Free shipping"}
+Else
+{
+$ShippingCost = 1.2 * $CustomerBasketWeight
+$TotalCost = $ShippingCost + $CustomerBasketCost
+$TotalCost
+}
+
+customer_basket_cost = 101
+customer_basket_weight = 44
+
+if customer_basket_cost > 100:
+  print(customer_basket_cost)
+else:
+  shipping = 1.20 * customer_basket_weight
+  total = customer_basket_cost + shipping
+  print(total)
+$CustomerBasketCost = 101
+$CustomerBasketWeight = 44
+
+If($CustomerBasketCost -gt 100)
+{Write-Host "Free shipping"}
+Else
+{
+$ShippingCost = 1.2 * $CustomerBasketWeight
+$TotalCost = $ShippingCost + $CustomerBasketCost
+$TotalCost
+}
+
